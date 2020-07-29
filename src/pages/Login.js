f4fd7b81f1d5422e7162a00c5dd843b417d541cc
jasonbrandoo <jasonbrandoo@gmail.com>
2020-07-30 01:31:52 +0700

    add eslint & prettier, ui update on register and login pages, refactor code on register and login pages.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,81 +1,91 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { setStorage } from "../utils/helpers";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { setStorage } from '../utils/helpers';
 
 function Login() {
-  const [data, setData] = useState({});
-  const [auth, isAuth] = useState(false);
-  const [errMessage, setMessage] = useState("");
+  const [data, setData] = useState({
+    username: '',
+    password: '',
+  });
+  const [isError, setIsError] = useState(false);
+  const [errMessage, setErrMessage] = useState('');
 
-  const login = async (e) => {
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios
-      .post(`${process.env.REACT_APP_API_URL}/auth/login`, {
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, {
         username: data.username,
         password: data.password,
-      })
-      .then((response) => {
-        setStorage("login", JSON.stringify(response.data));
-        window.location.href = "/home";
-        console.log(response.data);
-      })
-      .catch((error) => {
-        let Err = error.response;
-        if (Err === 401) {
-          isAuth(true);
-          setMessage(error.response.data.message);
-          console.log(error);
-        }
       });
+      setStorage('login', JSON.stringify(response.data));
+      window.location.href = '/home';
+    } catch (error) {
+      setIsError(true);
+      setErrMessage(error.response.data.message);
+    }
   };
+
   return (
-    <div className="h-screen">
-      <div className="flex flex-col justify-center h-screen items-center">
-        {auth ? (
-          <div className="bg-red-700 text-white rounded w-full sm:w-6/12 md:w-2/5 lg:w-4/12 xl:w-1/4 p-4">
-            {errMessage}
-          </div>
-        ) : null}
-        <form className="bg-white rounded px-8 pt-6 pb-8 mb-4 w-full sm:w-6/12 md:w-2/5 lg:w-4/12 xl:w-1/4">
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h2 className="text-4xl font-bold antialiased mb-2">WebChat</h2>
+      {isError && (
+        <div className="bg-red-500 text-white rounded w-7/12 sm:w-5/12 md:w-4/12 lg:w-3/12 mb-4 p-4">
+          {errMessage}
+        </div>
+      )}
+      <div className="w-7/12 sm:w-5/12 md:w-4/12 lg:w-3/12 flex flex-col items-center border rounded">
+        <form className="bg-white rounded px-8 pt-6 w-full" onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
               Username
             </label>
             <input
-              onChange={(e) => setData({ ...data, username: e.target.value })}
+              onChange={handleChange}
+              value={data.username}
               type="text"
               className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
+              id="username"
+              name="username"
               placeholder="Username"
+              required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
               Password
             </label>
             <input
-              onChange={(e) => setData({ ...data, password: e.target.value })}
+              onChange={handleChange}
+              value={data.password}
               type="password"
               className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
-              placeholder="Username"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
             />
           </div>
           <div className="mb-4">
             <button
-              type="button"
-              onClick={(e) => login(e)}
+              type="submit"
               className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
               Sign In
             </button>
           </div>
         </form>
-        <div className="flex-none">
-          <Link to="/register" className="text-blue-500">
-            Register
-          </Link>
-        </div>
+        <Link to="/register" className="text-blue-500 pb-6">
+          Register
+        </Link>
       </div>
     </div>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,73 +1,124 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Register() {
-    const [data, setData] = useState({});
-    const [auth, isAuth] = useState(false);
-    const [errMessage, setMessage] = useState("");
+  const [data, setData] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [isError, setIsError] = useState(null);
+  const [message, setMessage] = useState('');
 
-    var submit = async (e) => {
-        e.preventDefault();
-        console.log("test");
-        await axios
-            .post(`${process.env.REACT_APP_API_URL}/auth/register`, {
-                username: data.username,
-                password: data.password,
-            })
-            .then((response) => {
-                isAuth(true);
-                setMessage(response.data.message);
-            })
-            .catch((error) => {
-                let Err = error.response.data.status;
-                if (Err === 401) {
-                    isAuth(true);
-                    setMessage(error.response.data.message);
-                    console.log(error);
-                }
-            });
-    };
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        <div className="h-screen">
-            <div className="flex flex-col justify-center h-screen items-center">
-                {auth ? <div className={`bg-green-700 text-white rounded w-full sm:w-6/12 md:w-2/5 lg:w-4/12 xl:w-1/4 p-4`}>{errMessage}</div> : null}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (data.password !== data.confirmPassword) {
+      setIsError(true);
+      setMessage("Password didn't match");
+      return;
+    }
 
-                <form className="bg-white rounded px-8 pt-6 pb-8 mb-4 w-full sm:w-6/12 md:w-2/5 lg:w-4/12 xl:w-1/4" onSubmit={(e) => submit(e)}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-                        <input
-                            onChange={(e) => setData({ ...data, username: e.target.value })}
-                            type="text"
-                            name="username"
-                            className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                        <input
-                            onChange={(e) => setData({ ...data, password: e.target.value })}
-                            type="password"
-                            name="password"
-                            className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                            Sign Up
-                        </button>
-                    </div>
-                </form>
-                <div className="flex-none">
-                    <Link to="/" className="text-blue-500">
-                        Login
-                    </Link>
-                </div>
-            </div>
-        </div>
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/register`, {
+        username: data.username,
+        password: data.password,
+      });
+      setIsError(false);
+      setMessage(`${response.data.message} now you can login`);
+    } catch (error) {
+      setIsError(true);
+      setMessage(error.res.data.message);
+    }
+  };
+
+  let msg = null;
+
+  if (isError) {
+    msg = (
+      <div className="bg-red-500 text-white rounded w-7/12 sm:w-5/12 md:w-4/12 lg:w-3/12 mb-4 p-4">
+        {message}
+      </div>
     );
+  } else {
+    msg = (
+      <div className="bg-green-500 text-white rounded w-7/12 sm:w-5/12 md:w-4/12 lg:w-3/12 mb-4 p-4">
+        {message}
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h2 className="text-4xl font-bold antialiased mb-2">WebChat</h2>
+      {isError !== null && msg}
+      <div className="w-7/12 sm:w-5/12 md:w-4/12 lg:w-3/12 flex flex-col items-center border rounded">
+        <form className="bg-white rounded px-8 pt-6 w-full" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              onChange={handleChange}
+              value={data.username}
+              type="text"
+              className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
+              id="username"
+              name="username"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              onChange={handleChange}
+              value={data.password}
+              type="password"
+              className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
+              id="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <input
+              onChange={handleChange}
+              value={data.confirmPassword}
+              type="password"
+              className="p-2 rounded border-0 w-full focus:outline-none bg-gray-200"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+        <Link to="/" className="text-blue-500 pb-6">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 }
